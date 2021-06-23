3e5d53bdab6f10607e8ca1325204272b018fcb82
Abdul Lamzini <abd.ll@hotmail.com>
2021-06-23 10:41:01 +0100

    User can add profile picture during account creation

--- app/src/main/java/abdullamzini/com/myapplication/Auth/CreateAccountActivity.kt
+++ app/src/main/java/abdullamzini/com/myapplication/Auth/CreateAccountActivity.kt
@@ -1,12 +1,14 @@
 package abdullamzini.com.myapplication.Auth
 
 import abdullamzini.com.myapplication.R
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -14,6 +16,8 @@ import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.google.firebase.functions.FirebaseFunctions
 import com.google.firebase.functions.ktx.functions
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
@@ -22,12 +26,20 @@ class CreateAccountActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var functions: FirebaseFunctions
+    private lateinit var storage: FirebaseStorage
+    private lateinit var storageReference: StorageReference
 
     private lateinit var email: EditText
     private lateinit var password: EditText
     private lateinit var displayName: EditText
     private lateinit var phone: EditText
     private lateinit var submitBtn: Button
+    private lateinit var imageBtn: Button
+    private lateinit var imageView: ImageView
+
+    // Image select var
+    private var filePath: Uri? = null
+    private val PICK_IMAGE_REQUEST = 71
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,13 +48,20 @@ class CreateAccountActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
         functions = Firebase.functions
+        storage = FirebaseStorage.getInstance()
+        storageReference = storage.reference
 
         email = findViewById(R.id.editTextTextEmailAddress)
         password = findViewById(R.id.editTextTextPassword)
         displayName = findViewById(R.id.editTextTextPersonName)
         phone = findViewById(R.id.editTextPhone)
         submitBtn = findViewById(R.id.submitBtn)
+        imageBtn = findViewById(R.id.imageSelectButton)
+        imageView = findViewById(R.id.imageView)
 
+        imageBtn.setOnClickListener {
+            selectImage()
+        }
 
         submitBtn.setOnClickListener {
             val name: String = displayName.text.toString()
@@ -64,6 +83,24 @@ class CreateAccountActivity : AppCompatActivity() {
         }
     }
 
+    private fun selectImage() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(
+            Intent.createChooser(intent, "Select a building site Image"),
+            PICK_IMAGE_REQUEST
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            filePath = data?.data
+            imageView.setImageURI(filePath)
+        }
+    }
+
     private fun createAccount(name: String, mail: String, pass: String, phone: String) {
 
         auth.createUserWithEmailAndPassword(mail, pass)
@@ -74,23 +111,35 @@ class CreateAccountActivity : AppCompatActivity() {
                         displayName = name
                         photoUri = Uri.parse("https://example.com/jane-q-user/profile.jpg")
                     }
-                    user!!.updateProfile(profileUpdates)
-                        .addOnCompleteListener { task ->
-                            if (task.isSuccessful) {
-                                val updatedUser = auth.currentUser
-                                val data = hashMapOf(
-                                    "name" to (updatedUser?.displayName ?: "empty"),
-                                    "url" to "https://example.com/jane-q-user/profile.jpg",
-                                    "phone" to phone,
-                                    "push" to true
-                                )
-                                functions.getHttpsCallable("updateUser")
-                                    .call(data)
-                                if (updatedUser != null) {
-                                    Log.d("UpdateProfile", "User profile updated with name ${updatedUser.displayName}")
+                    if(filePath != null) {
+                        val imageRef: StorageReference =
+                            storageReference.child("${user?.uid}/images/profilePic.jpg")
+                        imageRef.putFile(filePath!!).addOnSuccessListener {
+                            user!!.updateProfile(profileUpdates)
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        val updatedUser = auth.currentUser
+                                        val data = hashMapOf(
+                                            "name" to (updatedUser?.displayName ?: "empty"),
+                                            "url" to filePath.toString(),
+                                            "phone" to phone,
+                                            "push" to true
+                                        )
+                                        functions.getHttpsCallable("updateUser")
+                                            .call(data)
+                                            .continueWith {
+                                                if (updatedUser != null) {
+                                                    Log.d(
+                                                        "UpdateProfile",
+                                                        "User profile updated with name ${updatedUser.displayName}"
+                                                    )
+                                                }
+                                            }
+                                    }
                                 }
-                            }
                         }
+                    }
+
                 } else {
                     Toast.makeText(baseContext, "Authentication failed.",
                         Toast.LENGTH_SHORT).show()
